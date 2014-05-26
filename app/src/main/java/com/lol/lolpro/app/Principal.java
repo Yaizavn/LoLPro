2134b45d94f137be89642964095f15d02bb8df28
Sergio Iglesias <crispiglesias7@gmail.com>
2014-05-26 19:03:54 +0200

    commit sergio

--- app/src/main/java/com/lol/lolpro/app/Principal.java
+++ app/src/main/java/com/lol/lolpro/app/Principal.java
@@ -14,7 +14,7 @@ public class Principal extends ActionBarActivity {
         setContentView(R.layout.activity_principal);
     }
 
-//HOLA; PROBANDO
+//HOLA; PROBANDO Probando
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
