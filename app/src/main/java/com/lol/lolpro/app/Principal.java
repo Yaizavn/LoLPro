1c108572354a50b543f0442b0b33102689667d4f
Sergio Iglesias <crispiglesias7@gmail.com>
2014-05-27 15:25:11 +0200

    Menu (no tested)

--- app/src/main/java/com/lol/lolpro/app/Principal.java
+++ app/src/main/java/com/lol/lolpro/app/Principal.java
@@ -1,20 +1,35 @@
 package com.lol.lolpro.app;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class Principal extends ActionBarActivity {
+
+    private String[] mItems;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        mItems = new String[]{"Campeones", "Objetos", "Temporizadores"};
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.MenuIzquierda);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mItems));
+
+        //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
-//HOLA; PROBANDO Probando
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
