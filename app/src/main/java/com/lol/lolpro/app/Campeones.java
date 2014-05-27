c0c42520ac6f6d1e3b89169ef1b8eec2e27c4714
Sergio Iglesias <crispiglesias7@gmail.com>
2014-05-27 17:44:26 +0200

    Menu2 (no tested)

---
+++ app/src/main/java/com/lol/lolpro/app/Campeones.java
@@ -0,0 +1,15 @@
+package com.lol.lolpro.app;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by sergio on 27/05/14.
+ */
+public class Campeones extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_campeones);
+    }
+}
--- app/src/main/java/com/lol/lolpro/app/Principal.java
+++ app/src/main/java/com/lol/lolpro/app/Principal.java
@@ -1,12 +1,16 @@
 package com.lol.lolpro.app;
 
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class Principal extends ActionBarActivity {
@@ -20,14 +24,40 @@ public class Principal extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        mItems = new String[]{"Campeones", "Objetos", "Temporizadores"};
+        mItems = getResources().getStringArray(R.array.titulosMenuIzquierda);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.MenuIzquierda);
 
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, mItems));
 
-        //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int posicion, long l) {
+                MostrarLayout(posicion);
+            }
+        });
+    }
+
+    private void MostrarLayout(int posicion){
+        Intent intent = null;
+        switch (posicion){
+            case 1:
+                intent = new Intent (Principal.this, Campeones.class);
+                break;
+            case 2:
+                intent = new Intent (Principal.this, Objetos.class);
+                break;
+            case 3:
+                intent = new Intent (Principal.this, Campeones.class);
+                break;
+            default:
+                Toast.makeText(getApplicationContext(), "Opcion no disponible", Toast.LENGTH_SHORT).show();
+        }
+        if (intent!=null) {
+            startActivity(intent);
+        }
     }
 
     @Override
