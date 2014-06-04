0c77225af10004420ea88a392fc3eb5f4bed7315
yaiza <yaizavn@gmail.com>
2014-06-04 12:59:18 +0200

    Objeto info previsualizacion errors fixed

--- app/src/main/java/com/lol/lolpro/app/ObjetoInfo.java
+++ app/src/main/java/com/lol/lolpro/app/ObjetoInfo.java
@@ -2,10 +2,14 @@ package com.lol.lolpro.app;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class ObjetoInfo extends Fragment {
 
@@ -25,15 +29,26 @@ public class ObjetoInfo extends Fragment {
         View view= inflater.inflate(R.layout.fragment_objeto_info, container, false);
         Bundle args = getArguments();
         int id = args.getInt("id", -1);
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getActivity().getResources().getDisplayMetrics());
         if (id!=-1){
             BBDDHelper helper= new BBDDHelper(getActivity());
             datos=helper.obtenerDatosObjetos(id);
+            String tienda="Si";
+            if (datos[4].compareTo("0")==0){
+                tienda="No";
+            }
             ((TextView) view.findViewById(R.id.nombre)).setText(datos[0]);
             ((TextView) view.findViewById(R.id.costeBase)).setText(datos[1]);
             ((TextView) view.findViewById(R.id.coste)).setText(datos[2]);
             ((TextView) view.findViewById(R.id.descripcion)).setText(datos[3]);
-            ((TextView) view.findViewById(R.id.puedesComprar)).setText(datos[4]);
-            ((TextView) view.findViewById(R.id.resumen)).setText(datos[5]);
+            ((TextView) view.findViewById(R.id.puedesComprar)).setText(tienda);
+            Picasso.with(getActivity()) //
+                    .load(datos[5]) //
+                    .placeholder(R.drawable.abc_ab_bottom_solid_light_holo) //TODO imagen palceholder
+                    .error(R.drawable.abc_ab_bottom_solid_dark_holo) //TODO imagen error
+                    .resize(px,px)
+                    .centerCrop() // Keep proportion
+                    .into((ImageView) view.findViewById(R.id.Imagen));
         }
         // Inflate the layout for this fragment
         return view;
